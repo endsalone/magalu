f8a47333e34d591dc381ebf78b4b3d37a226bb10
MaxLeandro14 <maxleandro2@gmail.com>
2019-09-29 16:44:05 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -20,16 +20,13 @@ const routes = [
     children: [
       { path: '', component: () => import(/* webpackChunkname: "index" */ 'pages/Perfil.vue'), name: 'perfil' }
     ]
-<<<<<<< HEAD
   },
   {
     path: '/campanha_embalagem',
     component: () => import('layouts/Default.vue'),
     children: [
-      { path: '', component: () => import(/* webpackChunkname: "index" */ 'pages/campanha_embalagem.vue'), name: 'perfil' }
+      { path: '', component: () => import(/* webpackChunkname: "index" */ 'pages/campanha_embalagem.vue'), name: 'camapanh' }
     ]
-=======
->>>>>>> 4bc180916ec9dcf495b91ecee035095142b87c69
   }
 ]
 
